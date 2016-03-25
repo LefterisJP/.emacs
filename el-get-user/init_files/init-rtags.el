cdaa2be11b0edc63ef787ab7e58c486cc27ec729
Lefteris Karapetsas <lefteris@refu.co>
2016-03-25 01:22:02 +0100

    Fix completions with rtags

--- el-get-user/init_files/init-rtags.el
+++ el-get-user/init_files/init-rtags.el
@@ -9,13 +9,18 @@
 
 (rtags-enable-standard-keybindings c-mode-base-map "\C-xr")
 (rtags-start-process-maybe)
-(setq rtags-autostart-diagnostics t)
 
-;; enable completions with company-mode
+;; -- enable completions with company-mode
 (require 'company-rtags)
+;; Need both of these to start diagnostics, which are needed for completions
+(setq rtags-autostart-diagnostics t)
+(rtags-diagnostics)
+;; enable rtags completion and add the rtags backend
 (setq rtags-completions-enabled t)
 (eval-after-load 'company
   '(add-to-list 'company-backends 'company-rtags))
+;; Add C-tab as the company-complete binding for c/c++
+(define-key c-mode-base-map (kbd "<C-tab>") (function company-complete))
 
 (provide 'init-rtags)
 ;;; init-rtags ends here
