4efbb95dfe9cd2ca10500f46f5e07b16dc67e753
Lefteris Karapetsas <lefteris@refu.co>
2016-04-12 10:52:50 +0200

    Add all_projects.org for generic org refile

--- config/org/my-org-refile.el
+++ config/org/my-org-refile.el
@@ -7,6 +7,26 @@
 (require 'ido)
 
 (setq org-default-notes-file "~/org/refile.org")
+
+(defun lk-org-query-location ()
+  "Query for a location/headline inside an org file during org-refile.
+
+It considers only level 1 headings in the target org file as potential targets."
+  (let* ((org-refile-targets '((nil :maxlevel . 1)))
+         (hd (condition-case nil
+                 (car (org-refile-get-location nil nil t t))
+               (error (car org-refile-history)))))
+    (goto-char (point-min))
+    (outline-next-heading)
+    (if (re-search-forward
+         (format org-complex-heading-regexp-format (regexp-quote hd))
+         nil t)
+        (goto-char (point-at-bol))
+      (goto-char (point-max))
+      (or (bolp) (insert "\n"))
+      (insert "* " hd "\n")))
+    (end-of-line))
+
 ;; Capture templates for specific type of tasks
 (setq org-capture-templates
       (quote (("s" "slock.it Tasks" entry (file+headline  "~/org/work/slockit.org" "General Tasks")
@@ -14,7 +34,12 @@
               ("r" "refulang Tasks" entry (file+headline  "~/org/projects/refulang/refulang.org" "General Tasks")
                "* TODO %?\n%U\n%a\n" :kill-bufer t)
               ("p" "personal Tasks" entry (file+headline  "~/org/personal/personal.org" "General Tasks")
-               "* TODO %?\n%U\n%a\n" :kill-bufer t))))
+               "* TODO %?\n%U\n%a\n" :kill-bufer t)
+              ("d" "Development Environment - System Tasks" entry (file+headline  "~/org/dev_env/dev_env.org" "Tasks TODO")
+               "* TODO %?\n%U\n%a\n" :kill-bufer t)
+              ("g" "General Tasks for all Projects" entry (file+function  "~/org/projects/all_projects.org" lk-org-query-location)
+               "* TODO %?\n%U\n%a\n" :kill-bufer t)
+              )))
 
 ;; REFILING OPTIONS
 ; Targets include this file and any file contributing to the agenda - up to 9 levels deep
