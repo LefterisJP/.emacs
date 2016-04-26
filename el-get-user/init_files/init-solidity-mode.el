2034ae7dcbd4479478e463afbaf3c80cfe3646ca
Lefteris Karapetsas <lefteris@refu.co>
2016-04-26 18:41:04 +0200

    modify solidity mode setup to follow latest package changes

--- el-get-user/init_files/init-solidity-mode.el
+++ el-get-user/init_files/init-solidity-mode.el
@@ -1,4 +1,3 @@
 (require 'flycheck)
-(require 'solidity-mode)
 (setq solidity-solc-path "/home/lefteris/ew/solidity/build/solc/solc")
-(add-to-list 'flycheck-checkers 'solidity-checker)
+(require 'solidity-mode)
