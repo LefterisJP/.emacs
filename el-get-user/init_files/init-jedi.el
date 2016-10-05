a383d96baa58311a76df104178ec407c2b2f0312
Lefteris Karapetsas <lefteris@refu.co>
2016-10-05 15:52:26 +0200

    Initialize ropemacs and jedi

--- el-get-user/init_files/init-jedi.el
+++ el-get-user/init_files/init-jedi.el
@@ -1,4 +1,15 @@
+;;; jedi --- My configuration for the python jedi package
+;;; Commentary:
+;;; Jedi is an autocompletion/static analysis library
+;;; for python
+;;; https://github.com/davidhalter/jedi
+;;;
+
+;;; Code:
 (require 'jedi)
 
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
+
+(provide 'init-jedi)
+;;; init-jedi ends here
