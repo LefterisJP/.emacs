9c51c37545a3dae9d9b478894231b6644f394573
Lefteris Karapetsas <lefteris@refu.co>
2016-10-12 17:45:52 +0200

    Fix el-get's solidity mode init file

--- el-get-user/init_files/init-solidity-mode.el
+++ el-get-user/init_files/init-solidity-mode.el
@@ -1,3 +1,10 @@
+;;; init-solidity-mode --- my solidity-mode configuration
+;;; Commentary:
+;; Github: https://github.com/ethereum/emacs-solidity
+
+;;; Code:
 (require 'flycheck)
 (setq solidity-solc-path "/home/lefteris/ew/solidity/build/solc/solc")
-(require 'solidity-mode)
+
+(provide 'init-solidity-mode)
+;;; init-solidity-mode.el ends here
