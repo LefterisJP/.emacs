f0204f8484556dbe708bf7ed51c2623c65fb1f12
Lefteris Karapetsas <lefteris@refu.co>
2017-02-12 16:51:10 +0100

    Add better rtags keybindings. Delete old projects

--- el-get-user/init_files/init-rtags.el
+++ el-get-user/init_files/init-rtags.el
@@ -19,8 +19,14 @@
 (setq rtags-completions-enabled t)
 (eval-after-load 'company
   '(add-to-list 'company-backends 'company-rtags))
+
 ;; Add C-tab as the company-complete binding for c/c++
 (define-key c-mode-base-map (kbd "<C-tab>") (function company-complete))
+
+;; Configure easy to use key bindings for rtags
+(define-key c-mode-base-map (kbd "M-.") (function rtags-find-symbol-at-point))
+(define-key c-mode-base-map (kbd "M-,") (function rtags-location-stack-back))
+(define-key c-mode-base-map (kbd "M-?") (function rtags-find-references-at-point))
 
 (provide 'init-rtags)
 ;;; init-rtags ends here
--- lk/malinka-projects.el
+++ lk/malinka-projects.el
@@ -24,60 +24,12 @@
  :test-cmd "./test.sh")
 
 (malinka-define-project
- :name "libweb3core"
- :root-directory "/home/lefteris/ew/libweb3core"
- :build-directory "/home/lefteris/ew/libweb3core/build"
- :configure-cmd "cmake .. -DCMAKE_BUILD_TYPE=Debug"
- :compile-cmd "make -j4"
- :test-cmd "NOTESTS")
-
-(malinka-define-project
- :name "libethereum"
- :root-directory "/home/lefteris/ew/libethereum"
- :build-directory "/home/lefteris/ew/libethereum/build"
- :configure-cmd "cmake .. -DCMAKE_BUILD_TYPE=Debug"
- :compile-cmd "make -j4"
- :test-cmd "./test/testeth")
-
-(malinka-define-project
- :name "webthree"
- :root-directory "/home/lefteris/ew/webthree"
- :build-directory "/home/lefteris/ew/webthree/build"
- :configure-cmd "cmake .. -DCMAKE_BUILD_TYPE=Debug"
- :compile-cmd "make -j4"
- :test-cmd "./test/testweb3")
-
-(malinka-define-project
  :name "solidity"
  :root-directory "/home/lefteris/ew/solidity"
  :build-directory "/home/lefteris/ew/solidity/build"
  :configure-cmd "cmake .. -DCMAKE_BUILD_TYPE=Debug"
  :compile-cmd "make -j4"
  :test-cmd "./test/soltest")
-
-(malinka-define-project
- :name "alethzero"
- :root-directory "/home/lefteris/ew/alethzero"
- :build-directory "/home/lefteris/ew/alethzero/build"
- :configure-cmd "cmake .. -DCMAKE_BUILD_TYPE=Debug"
- :compile-cmd "make -j4"
- :test-cmd "NOTESTS")
-
-(malinka-define-project
- :name "mix"
- :root-directory "/home/lefteris/ew/mix"
- :build-directory "/home/lefteris/ew/mix/build"
- :configure-cmd "cmake .. -DCMAKE_BUILD_TYPE=Debug"
- :compile-cmd "make -j4"
- :test-cmd "NOTESTS")
-
-(malinka-define-project
- :name "ethash"
- :root-directory "/home/lefteris/ew/ethash"
- :build-directory "/home/lefteris/ew/ethash/build"
- :configure-cmd "cmake .."
- :compile-cmd "make -j4"
- :test-cmd "cd /home/lefteris/ew/ethash && ./test/c/test.sh")
 
 (provide 'malinka-projects)
 ;;; malinka-projects.el ends here
