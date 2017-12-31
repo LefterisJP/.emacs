96fa045c21bb05e2ff6f44c41907f2cab5bf02d5
Lefteris Karapetsas <lefteris@refu.co>
2017-12-31 13:29:11 +0100

    Add solidity mode changes

--- el-get-user/init_files/init-solidity-mode.el
+++ el-get-user/init_files/init-solidity-mode.el
@@ -5,6 +5,11 @@
 ;;; Code:
 (require 'flycheck)
 (setq solidity-solc-path "/home/lefteris/ew/solidity/build/solc/solc")
+(setq solidity-solium-path "/home/lefteris/.npm-global/bin/solium")
+(setq solidity-flycheck-solc-checker-active t)
+(setq solidity-flycheck-solium-checker-active t)
+(setq flycheck-solidity-solium-soliumrcfile "/home/lefteris/.emacs.d/solidity/.soliumrc.json")
 (require 'solidity-mode)
+(setq solidity-comment-style 'slash)
 (provide 'init-solidity-mode)
 ;;; init-solidity-mode.el ends here
