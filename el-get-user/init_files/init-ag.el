f2cb1ac4d64e7cff0ffbb7fd6ec4f2c9a9ff3967
Lefteris Karapetsas <lefteris@refu.co>
2018-03-15 18:53:29 +0100

    Fix problems with ag search

--- el-get-user/init_files/init-ag.el
+++ el-get-user/init_files/init-ag.el
@@ -12,13 +12,15 @@
 (setq ag-highlight-search t)
 ;; Don't keep old search buffers around
 (setq ag-reuse-buffers 't)
+;; Don't group same file results -- breaks jumping to files
+(setq ag-group-matches nil)
 
 ;; these are the default arguments to ag, needed by the plugin
-(setq ag-arguments-default '("--smart-case" "--nogroup" "--column" "--"))
-
-(append (list "--ignore" "\"compile_commands.json\"") ag-arguments-default)
-(when *at-work*
-  (append (list "--ignore" "\"*.cfg\"" "--ignore" "\"*.html\"")
-          ag-arguments-default))
+(setq ag-arguments '(
+		     "--smart-case"
+		     "--nogroup"
+		     "--column"
+		     "--ignore" "compile_commands.json"
+		     "--"))
 (provide 'init-ag)
 ;;; init-ag.el ends here
