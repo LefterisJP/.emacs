ccec8c5e33c1afc4755dd2cd11f1a8d9572fe1e4
Lefteris Karapetsas <lefteris@refu.co>
2020-01-12 17:44:32 +0100

    Proper solidity mode setup for flycheck

--- el-get-user/init_files/init-solidity-mode.el
+++ el-get-user/init_files/init-solidity-mode.el
@@ -4,12 +4,18 @@
 
 ;;; Code:
 (require 'flycheck)
-(setq solidity-solc-path "/home/lefteris/ew/solidity/build/solc/solc")
+(setq solidity-solc-path "/home/lefteris/.local/bin/solc")
 (setq solidity-solium-path "/home/lefteris/.npm-global/bin/solium")
 (setq solidity-flycheck-solc-checker-active t)
-(setq solidity-flycheck-solium-checker-active t)
+;; (setq solidity-flycheck-solium-checker-active t)
 (setq flycheck-solidity-solium-soliumrcfile "/home/lefteris/.emacs.d/solidity/.soliumrc.json")
+(require 'solidity-flycheck)
 (require 'solidity-mode)
 (setq solidity-comment-style 'slash)
+(add-hook 'solidity-mode-hook
+          (lambda ()
+            (set (make-local-variable 'company-backends)
+                 (append '((company-solidity company-capf company-dabbrev-code))
+                         company-backends))))
 (provide 'init-solidity-mode)
 ;;; init-solidity-mode.el ends here
