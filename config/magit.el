86b6d8e04692ee6fa3f6a9930ffae1185159214f
Lefteris Karapetsas <lefteris@refu.co>
2025-02-01 22:53:40 +0100

    Magit get gpg sign option

--- config/magit.el
+++ config/magit.el
@@ -8,4 +8,5 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 (setq magit-auto-revert-mode t)
 (setq magit-last-seen-setup-instructions "1.4.0")
+(setq transient-default-level 5)  ;; to get gpg sign option
 (setq magit-popup-use-prefix-argument  'default)
